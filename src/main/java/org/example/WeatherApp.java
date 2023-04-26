07661f2061c48f4037b4b941e4c27c3855a7fd8b
Sergo2292 <muhamet77737@gmail.com>
2023-04-26 18:05:11 +0300

    HW6

--- src/main/java/org/example/WeatherApp.java
+++ src/main/java/org/example/WeatherApp.java
@@ -2,6 +2,11 @@ package org.example;
 
 
 
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
